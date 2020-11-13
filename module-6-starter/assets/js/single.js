cc4a8141e42a5a4f6b90b6548187e4e112d777e1
mooka0 <angelankao@gmail.com>
2020-11-12 17:05:20 -0800

    add extra message

--- module-6-starter/assets/js/single.js
+++ module-6-starter/assets/js/single.js
@@ -1,3 +1,4 @@
+var limitWarningEl = document.querySelector("#limit-warning");
 var issueContainerEl = document.querySelector("#issues-container");
 
 var getRepoIssues = function (repo) {
@@ -14,9 +15,36 @@ var getRepoIssues = function (repo) {
             alert("There was a problem with your request!");
         }
     });
+
+    if (response.ok) {
+        response.json().then(function (data) {
+            displayIssues(data);
+
+            // check if api has paginated issues
+            if (response.headers.get("Link")) {
+                displayWarning(repo);
+            }
+        });
+    }
 };
 
 getRepoIssues("facebook/react");
+
+var displayWarning = function (repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    // append to warning container
+    limitWarningEl.appendChild(linkEl);
+
+
+};
+
+
 
 var displayIssues = function (issues) {
 
